c2c0dd0e05e7b31bbc858fd3b08c0c1ee5fa13de
diana <diana.r.tretyakova@gmail.com>
2023-03-05 23:58:13 +0500

    refactoring

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -5,10 +5,11 @@ $(document).ready(function () {
     const pageNumber = $("#page-number");
 
     function renderTable(pageSize, pageNumber) {
-        $.get("/rest/players/?pageNumber=0&pageSize=5", function (data) {
+        $.get(`/rest/players/?pageNumber=${pageNumber}&pageSize=${pageSize}`, function (data) {
+            tableBody.empty();
             $.each(data, function (i, member) {
                 const row = `<tr>
-                    <td>${i + 1}</td>
+                    <td>${i + data[0].id}</td>
                     <td>${member.name}</td>
                     <td>${member.title}</td>
                     <td>${member.race}</td>
@@ -39,40 +40,15 @@ $(document).ready(function () {
     renderTable(5, 0);
     renderPaginationButtons();
 
-
     dropDown.click(() => {
-
         let pageSize = dropDown.val();
-
-        $.get('/rest/players/?pageNumber=0&pageSize=' + pageSize + '', function (data) {
-            tableBody.empty();
-            $.each(data, function (i, members) {
-                const row = $("<tr><td>" + (i + 1) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
-                table.append(row);
-            });
-        });
-
-        $.get('/rest/players/count', function (data) {
-            let chunk = Math.ceil(data / pageSize);
-            pageNumber.empty();
-            for (let i = 1; i <= chunk; i++) {
-                let button = $('<button>' + i + '</button>');
-                pageNumber.append(button);
-                button.attr('id', 'b-num');
-                button.attr('value', i);
-            }
-        });
+        renderTable(pageSize, 0);
+        renderPaginationButtons();
     });
 
     pageNumber.on('click', 'button', function () {
         let pageSize = dropDown.val();
         let pageNumber = $(this).val() - 1;
-        $.get('/rest/players/?pageNumber=' + pageNumber + '&pageSize=' + pageSize + '', function (data) {
-            tableBody.empty();
-            $.each(data, function (i, members) {
-                const row = $("<tr><td>" + (i + data[0].id) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
-                table.append(row);
-            });
-        });
+        renderTable(pageSize, pageNumber);
     });
 });
