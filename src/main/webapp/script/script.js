ebf6eac5d0ce744c395c3e44673944c2dbea3af0
diana <diana.r.tretyakova@gmail.com>
2023-03-05 23:40:31 +0500

    refactoring

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,61 +1,76 @@
 $(document).ready(function () {
-    $.get("/rest/players/?pageNumber=0&pageSize=5", function (data) {
-        var table = $("#t-body");
-        $.each(data, function (i, members) {
-            var row = $("<tr><td>" + (i + 1) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
-            table.append(row);
+    const table = $("#my-table");
+    const tableBody = $("#t-body");
+    const dropDown = $("#my-dropdown");
+    const pageNumber = $("#page-number");
+
+    function renderTable(pageSize, pageNumber) {
+        $.get("/rest/players/?pageNumber=0&pageSize=5", function (data) {
+            $.each(data, function (i, member) {
+                const row = `<tr>
+                    <td>${i + 1}</td>
+                    <td>${member.name}</td>
+                    <td>${member.title}</td>
+                    <td>${member.race}</td>
+                    <td>${member.profession}</td>
+                    <td>${member.level}</td>
+                    <td>${member.birthday}</td>
+                    <td>${member.banned}</td>
+                    </tr>`;
+                table.append(row);
+            });
         });
-    });
+    }
 
-    $.get('/rest/players/count', function (data) {
-        let pageSize = $('#myDropdown').val();
-        let chunk = Math.ceil(data / pageSize);
-        const numberButton = document.getElementById('page-number');
-        numberButton.innerHTML = '';
-        for (let i = 1; i <= chunk; i++) {
-            let button = $('<button>' + i + '</button>');
-            $('#page-number').append(button);
-            button.attr('id', 'b-num');
-            button.attr('value', i);
-        }
-    });
-    $('#myDropdown').click(() => {
+    function renderPaginationButtons() {
+        $.get('/rest/players/count', function (data) {
+            let pageSize = dropDown.val();
+            let chunk = Math.ceil(data / pageSize);
+            pageNumber.empty();
+            for (let i = 1; i <= chunk; i++) {
+                let button = $('<button>' + i + '</button>');
+                pageNumber.append(button);
+                button.attr('id', 'b-num');
+                button.attr('value', i);
+            }
+        });
+    }
 
-        let pageSize = $('#myDropdown').val();
+    renderTable(5, 0);
+    renderPaginationButtons();
+
+
+    dropDown.click(() => {
+
+        let pageSize = dropDown.val();
 
         $.get('/rest/players/?pageNumber=0&pageSize=' + pageSize + '', function (data) {
-            var table = $('#my-table');
-            const t = document.getElementById('t-body');
-            t.innerHTML = '';
+            tableBody.empty();
             $.each(data, function (i, members) {
-                var row = $("<tr><td>" + (i + 1) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
+                const row = $("<tr><td>" + (i + 1) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
                 table.append(row);
             });
         });
 
         $.get('/rest/players/count', function (data) {
             let chunk = Math.ceil(data / pageSize);
-            const numberButton = document.getElementById('page-number');
-            numberButton.innerHTML = '';
+            pageNumber.empty();
             for (let i = 1; i <= chunk; i++) {
                 let button = $('<button>' + i + '</button>');
-                $('#page-number').append(button);
+                pageNumber.append(button);
                 button.attr('id', 'b-num');
                 button.attr('value', i);
             }
         });
     });
 
-    $('#page-number').on('click', 'button', function () {
-        let pageSize = $('#myDropdown').val();
+    pageNumber.on('click', 'button', function () {
+        let pageSize = dropDown.val();
         let pageNumber = $(this).val() - 1;
         $.get('/rest/players/?pageNumber=' + pageNumber + '&pageSize=' + pageSize + '', function (data) {
-            var table = $('#my-table');
-            const t = document.getElementById('t-body');
-            t.innerHTML = '';
+            tableBody.empty();
             $.each(data, function (i, members) {
-                console.log(data);
-                var row = $("<tr><td>" + (i + data[0].id) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
+                const row = $("<tr><td>" + (i + data[0].id) + "</td><td>" + members.name + "</td><td>" + members.title + "</td><td>" + members.race + "</td><td>" + members.profession + "</td><td>" + members.level + "</td><td>" + members.birthday + "</td><td>" + members.banned + "</td></tr>");
                 table.append(row);
             });
         });
